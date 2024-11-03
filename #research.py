daa97562793d46845d865030afb0c4f68d317efd
Shivani-369 <shivani63124@gmail.com>
2024-11-03 10:55:10 +0530

    dictionary
    used to classify the patients based on their criticality

---
+++ #research.py
@@ -0,0 +1,15 @@
+#research
+d={"Janaki Deva":"Medium","Hemant Ram":"Low","Hari Gopal":"High","Shruthika":"Medium"}
+m=[]
+for k,v in d.items():
+    if v=="High":
+        m.append(k)
+for k,v in d.items():
+    if v=="Medium":
+        m.append(k)
+for k,v in d.items():
+    if v=="Low":
+        m.append(k)
+print("priority list",m)
+
+
